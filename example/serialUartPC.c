9efde422357e4f5e6566d952f50661294ae45c02
leanhhuy <anhhuy@live.com>
2016-01-08 16:06:51 +0700

    add send char

--- example/serialUartPC.c
+++ example/serialUartPC.c
@@ -5,14 +5,26 @@
 Serial pc(SERIAL_TX, SERIAL_RX);
 DigitalIn mybutton(USER_BUTTON);
 DigitalOut myled(LED1);
+int hl='\0';
 int main()
 {
 	pc.printf("Hello HuyLe\n");
-	if (mybutton == 0) {
-		myled = 1;
-		pc.printf("Welcome HuyLe\n");
-	}else{
-		pc.printf("Please PRESSED Button now\n");
-		myled = 0;
+	while(1){
+		if (mybutton == 0) {
+			myled = 1;
+			pc.printf("Pressed button success\n");
+			// send char a to consoler uart2
+			pc.putc('a');
+		}else{
+			pc.printf("Please PRESSED Button now\n");
+			myled = 0;
+		}
+		if(pc.readable()){
+			pc.putc(pc.getc());
+			if(hl=='a'){
+				pc.printf("Send char a received from USB\n\r");
+			}
+			hl='\0';
+		}
 	}
 }
